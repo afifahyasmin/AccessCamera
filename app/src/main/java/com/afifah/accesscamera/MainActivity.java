c7cd373271c5489701226f2e2664a42f69f23036
afifahyasmin <afifahchan22@gmail.com>
2022-04-13 14:06:42 -0500

    Initial commit

---
+++ app/src/main/java/com/afifah/accesscamera/MainActivity.java
@@ -0,0 +1,25 @@
+package com.afifah.accesscamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCamera = findViewById(R.id.buttonCamera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+    }
+}
